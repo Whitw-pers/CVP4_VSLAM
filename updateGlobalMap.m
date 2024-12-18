15dead1a884d8c46965e74d1ede7f81c573cc612
Whitlcw <58365468+Whitlcw@users.noreply.github.com>
2024-12-18 11:06:54 -0700

    drafted updateGlobalMap.m

--- updateGlobalMap.m
+++ updateGlobalMap.m
@@ -1,6 +1,26 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
-% performs role of helperUpdateGlobalMap()
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function worldPointSet = updateGlobalMap(worldPointSet, keyframeSet, ...
+        optKeyframeSet)
+% update world points after pose graph optimization
+% INPUTS
+% OUTPUTS
+
+    oldPoses = keyframeSet.Views.AbsolutePose;
+    newPoses = optKeyframeSet. Views.AbsolutePose;
+    oldPoints = worldPointSet.WorldPoints;
+    newPoints = oldPoints;
+
+    % update loc of world points using new abs pose of corresponding major
+    % view
+    for i = 1:worldPointSet.Count
+
+        majorViewIDs = worldPointSet.RepresentativeViewId(i);
+        newPose = newPoses(majorViewIDs).A;
+        tform = affinetform3d(newPose/oldPoses(majorViewIDs).A);
+        newPoints(i, :) = transformPointsForward(tform, oldPoints(i, :)); 
+
+    end
+
+    worldPointSet = updateWorldPoints(worldPointSet, 1:worldPointSet.Count, newPoints);
+
 end
 
